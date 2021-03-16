7f6fea499af6f8c02360d5602395eb3c3ecc40e1
kavishamodi97 <kavishamodi971@gmail.com>
2021-03-16 23:09:05 +0530

    commit with implement Question service and Question Dao

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -1,6 +1,11 @@
 package com.upgrad.quora.service.business;
 
+import com.upgrad.quora.service.dao.AuthTokenDao;
+import com.upgrad.quora.service.dao.QuestionDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,8 +13,23 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class QuestionBusinessService {
 
+    @Autowired
+    private AuthTokenDao authTokenDao;
+
+    @Autowired
+    private QuestionDao questionDao;
+
     @Transactional(propagation = Propagation.REQUIRED)
-    public QuestionEntity createQuestion(final QuestionEntity questionEntity, final String accessToken) {
-        return null;
+    public QuestionEntity createQuestion(final QuestionEntity questionEntity, final String accessToken) throws AuthorizationFailedException {
+        UserAuthEntity userAuthTokenEntity = authTokenDao.checkAuthToken(accessToken);
+
+        if (userAuthTokenEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthTokenEntity.getLogoutTime() != null) {
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to post a question");
+        }
+
+        questionEntity.setUser(userAuthTokenEntity.getUser());
+        return questionDao.createQuestion(questionEntity);
     }
 }
