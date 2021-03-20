b687afe451c0cd7ed9262bce1cb6fd144514ddd2
adixit <adixit@wdtablesystems.com>
2021-03-20 17:18:07 +0530

    Created AnswerDao for Answer Feature.

---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -0,0 +1,23 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class AnswerDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    //This Dao method creates an answer in database
+    public AnswerEntity createAnswer(AnswerEntity answerEntity) {
+        entityManager.persist(answerEntity);
+        return answerEntity;
+    }
+}
